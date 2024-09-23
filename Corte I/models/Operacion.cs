87411680b1a33b234ce4313195691ab867ee142c
achitoo <hbaltodano@uamv.edu.ni>
2024-09-23 10:02:13 -0600

    profenolentendi

---
+++ Corte I/models/Operacion.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Corte_I.models
+{
+    internal class Operacion
+    {
+        public Operacion() { }
+        public Operacion(int id) { }
+        public int Id { get; set; }
+
+        public int Edad { get; set; }
+
+        if (edad += 18); 
+
+        
+
+    
+    }
+
+}
---
+++ Corte I/models/persona.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Corte_I.models
+{
+    internal class persona
+    {
+        public persona() { }
+        public persona(int id) { }
+        public int Id { get; set; }
+
+        persona(string name ) { }
+
+        persona(int id, string name) { }
+        
+
+
+    }
+}
